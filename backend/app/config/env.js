fa4f47335e836877084e7a6ce848be7ca8679dbf
Caio Pessoa <caio.almeida.pessoa07@aluno.ifce.edu.br>
2021-05-30 22:53:01 -0300

    criando projeto

---
+++ backend/app/config/env.js
@@ -0,0 +1,9 @@
+const env = {
+    database: 'db_backend',
+    username: 'root',
+    password: '123456',
+    host: 'localhost',
+    dialect: 'mysql',
+  };
+  
+  module.exports = env;
---
+++ frontend/projeto/src/pages/EditClientes/index.js
@@ -0,0 +1,104 @@
+import { Component } from "react";
+import api from '../../service/api';
+
+export default class EditCliente extends Component{
+    
+    state = {
+      id:this.props.match.params.id,
+      cad_nome:'',
+      cad_idade:'',
+      cad_email:'',
+    };
+
+    componentDidMount = async e =>{
+   
+      const response = await api.get(`/cliente/${this.state.id}`)
+      
+      this.setState({
+        cad_nome:response.data.nome,
+        cad_idade:response.data.idade,
+        cad_email:response.data.email,
+      });
+    
+      console.log(response);
+
+    }
+
+    handleNomeChange = e =>{
+      this.setState({cad_nome: e.target.value});
+    };
+
+    handleIdadeChange = e =>{
+      this.setState({cad_idade: e.target.value});
+    };
+
+    handleEmailChange = e =>{
+      this.setState({cad_email: e.target.value});
+    };
+
+    handleOnSubmit = async e =>{
+      const {id, cad_nome,cad_idade,cad_email} = this.state;  
+        e.preventDefault();
+
+        const Cliente = {'id': id,'nome':cad_nome,'idade':cad_idade,'email':cad_email}
+
+        await api.put(`/cliente`,Cliente)
+        .then(console.log(Cliente))
+
+        alert('usuário alterado com sucesso.');
+
+    }
+
+    render(){
+
+      const {id,cad_nome,cad_idade,cad_email} = this.state;
+
+      return(
+
+          <div className="container" > 
+            <div className="content">         
+              <div id="cadastro">
+                <form onSubmit={this.handleOnSubmit}> 
+                  <h1>Alterar cadastro</h1>
+                  <div className="novaClass">Testando</div>
+                  <div class="buttHover">
+                    <a type="submit" className="buttLink">
+                      <span data="S">S</span>
+                      <span data="U">U</span>
+                      <span data="J">J</span>
+                      <span data="E">E</span>
+                    </a>
+                  </div>
+                  <p> 
+                    <label >Id</label>
+                    <input type="number" value={id} readOnly/>
+                  </p>
+
+                  <p> 
+                    <label >Seu nome</label>
+                    <input required="required" type="text" placeholder="Digite seu nome"
+                        value={cad_nome} onChange={this.handleNomeChange}/>
+                  </p>
+
+                  <p> 
+                    <label>Sua idade</label>
+                    <input required="required" type="text" placeholder="Digite seu endereço"
+                        value={cad_idade} onChange={this.handleIdadeChange}/> 
+                  </p>                  
+                  
+                  <p> 
+                    <label>Seu email</label>
+                    <input required="required" type="text" placeholder="Digite sua idade"
+                        value={cad_email} onChange={this.handleEmailChange}/>
+                  </p>                 
+                  <p> 
+                    <input type="submit" value="Alterar"/> 
+                  </p>
+                </form>
+              </div>
+            </div>
+        </div>
+
+      )
+  }
+}
